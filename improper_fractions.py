efb4f79f54362ba63f084202dba6e7e3d125a356
Geneva33 <rogin3wang@gmail.com>
2022-01-28 09:45:56 +0800

    lab1

--- improper_fractions.py
+++ improper_fractions.py
@@ -1,7 +1,7 @@
 def main():
     a = input("Enter a floating number:")
     b = float(a) * 100
-    print("ACTUAL:", a, "is", int(b), "/100")
+    print("ACTUAL:", a, " ", "is", " ", int(b), "/100", sep = "")
 
 
 if __name__ == "__main__":
